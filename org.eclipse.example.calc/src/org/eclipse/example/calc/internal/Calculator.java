bd2215589ff482f7cbfb56893542f10a2d3b323b
Andreas von Benten <andreas.von.benten@ituv-software.de>
2019-07-03 17:00:55 +0200

    comment 2

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -36,7 +36,7 @@ public class Calculator {
 	}
 
 	private void setupDefaultOperations() {
-		// no more operation needed
+		// no more operation needed 2
 		new Equals();
 		new Minus();
 		new Plus();
